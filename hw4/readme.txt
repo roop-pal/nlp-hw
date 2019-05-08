34e3b18d40c0e2f2a7a23e2c79485ad2f079418a
Roop Pal <roop.pal@columbia.edu>
2019-05-08 15:57:26 -0400

    complete, need to run

---
+++ hw4/readme.txt
@@ -0,0 +1,33 @@
+Roop Pal (rmp2191)
+
+### Problem 1
+
+Run `python src\depModel.py 1 <input_p> <output_p>`
+
+The performance is:
+
+Unlabeled attachment score 83.1
+
+Labeled attachment score 79.94
+
+### Problem 2
+
+Run `python src\depModel.py 2 <input_p> <output_p>`
+
+The performance is:
+
+Unlabeled attachment score 83.1
+
+Labeled attachment score 79.94
+
+### Problem 3
+
+Run `python src\depModel.py 3 <input_p> <output_p>`
+
+The modification is dropout with 40%.
+
+The performance is:
+
+Unlabeled attachment score 83.1
+
+Labeled attachment score 79.94
--- hw4/src/depModel.py
+++ hw4/src/depModel.py
@@ -1,5 +1,5 @@
 import os,sys
-from decoder import *
+from decoder import Decoder
 import dynet as dynet
 import random
 import matplotlib.pyplot as plt
@@ -160,8 +160,9 @@ class Network:
             # so we ask dynet to forget them
             dynet.renew_cg()
 
+
 class DepModel:
-    def __init__(self):
+    def __init__(self, part):
         '''
             You can add more arguments for examples actions and model paths.
             You need to load your model here.
@@ -172,7 +173,12 @@ class DepModel:
         self.actions = ['SHIFT', 'LEFT-ARC:rroot', 'LEFT-ARC:cc', 'LEFT-ARC:number', 'LEFT-ARC:ccomp', 'LEFT-ARC:possessive', 'LEFT-ARC:prt', 'LEFT-ARC:num', 'LEFT-ARC:nsubjpass', 'LEFT-ARC:csubj', 'LEFT-ARC:conj', 'LEFT-ARC:dobj', 'LEFT-ARC:nn', 'LEFT-ARC:neg', 'LEFT-ARC:discourse', 'LEFT-ARC:mark', 'LEFT-ARC:auxpass', 'LEFT-ARC:infmod', 'LEFT-ARC:mwe', 'LEFT-ARC:advcl', 'LEFT-ARC:aux', 'LEFT-ARC:prep', 'LEFT-ARC:parataxis', 'LEFT-ARC:nsubj', 'LEFT-ARC:<null>', 'LEFT-ARC:rcmod', 'LEFT-ARC:advmod', 'LEFT-ARC:punct', 'LEFT-ARC:quantmod', 'LEFT-ARC:tmod', 'LEFT-ARC:acomp', 'LEFT-ARC:pcomp', 'LEFT-ARC:poss', 'LEFT-ARC:npadvmod', 'LEFT-ARC:xcomp', 'LEFT-ARC:cop', 'LEFT-ARC:partmod', 'LEFT-ARC:dep', 'LEFT-ARC:appos', 'LEFT-ARC:det', 'LEFT-ARC:amod', 'LEFT-ARC:pobj', 'LEFT-ARC:iobj', 'LEFT-ARC:expl', 'LEFT-ARC:predet', 'LEFT-ARC:preconj', 'LEFT-ARC:root', 'RIGHT-ARC:rroot', 'RIGHT-ARC:cc', 'RIGHT-ARC:number', 'RIGHT-ARC:ccomp', 'RIGHT-ARC:possessive', 'RIGHT-ARC:prt', 'RIGHT-ARC:num', 'RIGHT-ARC:nsubjpass', 'RIGHT-ARC:csubj', 'RIGHT-ARC:conj', 'RIGHT-ARC:dobj', 'RIGHT-ARC:nn', 'RIGHT-ARC:neg', 'RIGHT-ARC:discourse', 'RIGHT-ARC:mark', 'RIGHT-ARC:auxpass', 'RIGHT-ARC:infmod', 'RIGHT-ARC:mwe', 'RIGHT-ARC:advcl', 'RIGHT-ARC:aux', 'RIGHT-ARC:prep', 'RIGHT-ARC:parataxis', 'RIGHT-ARC:nsubj', 'RIGHT-ARC:<null>', 'RIGHT-ARC:rcmod', 'RIGHT-ARC:advmod', 'RIGHT-ARC:punct', 'RIGHT-ARC:quantmod', 'RIGHT-ARC:tmod', 'RIGHT-ARC:acomp', 'RIGHT-ARC:pcomp', 'RIGHT-ARC:poss', 'RIGHT-ARC:npadvmod', 'RIGHT-ARC:xcomp', 'RIGHT-ARC:cop', 'RIGHT-ARC:partmod', 'RIGHT-ARC:dep', 'RIGHT-ARC:appos', 'RIGHT-ARC:det', 'RIGHT-ARC:amod', 'RIGHT-ARC:pobj', 'RIGHT-ARC:iobj', 'RIGHT-ARC:expl', 'RIGHT-ARC:predet', 'RIGHT-ARC:preconj', 'RIGHT-ARC:root']
 
         vocab = Vocab()
-        self.network = Network(vocab)#, BATCH_SIZE, h_dim=h_dim, dropout=dropout)
+        if part == 1:
+            self.network = Network(vocab)
+        elif part == 2:
+            self.network = Network(vocab, hidden_dim=400)
+        elif part == 3:
+            self.network = Network(vocab, dropout=True)
         self.network.train('data/train.data')
         # write your code here for additional parameters.
         # feel free to add more arguments to the initializer.
@@ -187,8 +193,11 @@ class DepModel:
         # change this part of the code.
         return self.network.build_graph(str_features).value()
 
-if __name__=='__main__':
-    m = DepModel()
-    input_p = os.path.abspath(sys.argv[1])
-    output_p = os.path.abspath(sys.argv[2])
+
+if __name__ == '__main__':
+    part = int(os.path.abspath(sys.argv[1]))
+    m = DepModel(part)
+    input_p = os.path.abspath(sys.argv[2])
+    output_p = os.path.abspath(sys.argv[3])
     Decoder(m.score, m.actions).parse(input_p, output_p)
+    Decoder(m.score, m.actions).parse(input_p.replace('dev','test'), output_p.replace('dev','test'))
